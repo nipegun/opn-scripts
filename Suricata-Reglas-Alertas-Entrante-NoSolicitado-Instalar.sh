d5e3aeb3382e177ffcbec6c995340e6e50a8dd24
NiPeGun <nipegun@gmail.com>
2023-02-04 13:25:34 +0100

    Update Suricata-Reglas-Alertas-Entrante-NoSolicitado-Instalar.sh

--- Suricata-Reglas-Alertas-Entrante-NoSolicitado-Instalar.sh
+++ Suricata-Reglas-Alertas-Entrante-NoSolicitado-Instalar.sh
@@ -12,7 +12,7 @@
 #  curl -s https://raw.githubusercontent.com/nipegun/opn-scripts/master/Suricata-Reglas-Alertas-Entrante-NoSolicitado-Instalar.sh | bash
 # ----------
 
-touch /usr/local/etc/suricata/entrante-no-solicitado.rules 2> /dev/null
+touch /usr/local/etc/suricata/opnsense.rules/entrante-no-solicitado.rules 2> /dev/null
 echo 'alert icmp $EXTERNAL_NET any -> $HOME_NET any (msg: "Detectada conexión ICMP entrante no solicitada"; flow:not_established; sid:5000001; rev:1;)' >> /usr/local/etc/suricata/opnsense.rules/entrante-no-solicitado.rules
 echo 'alert tcp $EXTERNAL_NET any -> $HOME_NET any (msg: "Detectada conexión TCP entrante no solicitada"; flow:not_established; sid:5000002; rev:1;)'   >> /usr/local/etc/suricata/opnsense.rules/entrante-no-solicitado.rules
 echo 'alert udp $EXTERNAL_NET any -> $HOME_NET any (msg: "Detectada conexión UDP entrante no solicitada"; flow:not_established; sid:5000003; rev:1;)'   >> /usr/local/etc/suricata/opnsense.rules/entrante-no-solicitado.rules
