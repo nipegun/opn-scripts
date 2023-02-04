8cb5cfeab85071d039d15e224e38ac071ef6918f
NiPeGun <nipegun@gmail.com>
2023-02-04 13:47:32 +0100

    Update Suricata-Fuente-hacks4geeks-Instalar.sh

--- Suricata-Fuente-hacks4geeks-Instalar.sh
+++ Suricata-Fuente-hacks4geeks-Instalar.sh
@@ -2,27 +2,19 @@
 
 # Crear el directorio
  mkdir -p /usr/local/opnsense/scripts/suricata/metadata/rules/ 2> /dev/null
-# Crear el archivo
-  touch /usr/local/opnsense/scripts/suricata/metadata/rules/custom.xml 2> /dev/null
-# Rellenar el archivo
-  echo '<?xml version="1.0"?>'                                                                      >> /usr/local/opnsense/scripts/suricata/metadata/rules/custom.xml
-  echo '<ruleset documentation_url="http://docs.opnsense.org/">'                                    >> /usr/local/opnsense/scripts/suricata/metadata/rules/custom.xml
-  echo '    <location url="https://hacks4geeks.com/_/premium/opnsense/suricata/" prefix="Custom"/>' >> /usr/local/opnsense/scripts/suricata/metadata/rules/custom.xml
-  echo '    <files>'                                                                                >> /usr/local/opnsense/scripts/suricata/metadata/rules/custom.xml
-  echo '        <file description="custom rules">Custom.rules</file>'                               >> /usr/local/opnsense/scripts/suricata/metadata/rules/custom.xml
-  echo '        <file description="Custom" url="inline::rules/Custom.rules">Custom.rules</file>'    >> /usr/local/opnsense/scripts/suricata/metadata/rules/custom.xml
-  echo '    </files>'                                                                               >> /usr/local/opnsense/scripts/suricata/metadata/rules/custom.xml
-  echo '</ruleset>'                                                                                 >> /usr/local/opnsense/scripts/suricata/metadata/rules/custom.xml
-#
-
+# Crear el archivo (por si no está creado)
   touch /usr/local/opnsense/scripts/suricata/metadata/rules/hacks4geeks.xml 2> /dev/null
+# Vaciarlo
+  truncate -s 0 /usr/local/opnsense/scripts/suricata/metadata/rules/hacks4geeks.xml 2> /dev/null
 # Rellenar el archivo
   echo '<?xml version="1.0"?>'                                                                             >> /usr/local/opnsense/scripts/suricata/metadata/rules/custom.xml
   echo '<ruleset documentation_url="http://docs.opnsense.org/">'                                           >> /usr/local/opnsense/scripts/suricata/metadata/rules/custom.xml
-  echo '    <location url="https://hacks4geeks.com/_/premium/opnsense/suricata/" prefix="Custom"/>'        >> /usr/local/opnsense/scripts/suricata/metadata/rules/custom.xml
+  echo '    <location url="https://hacks4geeks.com/_/premium/opnsense/suricata/" prefix="hacks4geeks"/>'   >> /usr/local/opnsense/scripts/suricata/metadata/rules/custom.xml
   echo '    <files>'                                                                                       >> /usr/local/opnsense/scripts/suricata/metadata/rules/custom.xml
   echo '        <file description="hacks4geeks rules">hacks4geeks.rules</file>'                            >> /usr/local/opnsense/scripts/suricata/metadata/rules/custom.xml
   echo '        <file description="Custom" url="inline::rules/hacks4geeks.rules">hacks4geeks.rules</file>' >> /usr/local/opnsense/scripts/suricata/metadata/rules/custom.xml
   echo '    </files>'                                                                                      >> /usr/local/opnsense/scripts/suricata/metadata/rules/custom.xml
   echo '</ruleset>'                                                                                        >> /usr/local/opnsense/scripts/suricata/metadata/rules/custom.xml
 # 
+service suricata restart
+
